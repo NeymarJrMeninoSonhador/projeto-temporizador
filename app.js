6efbbf2ac4d98cd550b0374abe48b4bad25d8116
NeymarJrMeninoSonhador <pelosogabrieldeoliveirapeloso@gmail.com>
2026-05-04 16:53:47 -0300

    Input

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 const tempo = document.getElementById('tempo')
+const diminuirBtn = document.getElementById('diminuir')
+const aumentarBtn = document.getElementById('aumentar')
+const valorAtualInput = document.getElementById('valor-atual')
 
 let tempoRestante = 60
+let valorAtual = 75
+
+diminuirBtn.addEventListener('click' , () => {
+    if(valorAtual - 10 <= 0){
+        valorAtual = 1
+    } else {
+        valorAtual -= 10
+    }
+
+    valorAtualInput.value = valorAtual
+
+})
+
+aumentarBtn.addEventListener('click' , () => {
+valorAtual += 10
+
+   valorAtualInput.value =valorAtual
+})
 
 setInterval(() =>{
     
